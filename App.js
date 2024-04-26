bb6702ffc369eeb2b463c27cb5601ed7af299887
ghaith990000 <ghaith990000@gmail.com>
2024-04-26 17:06:11 +0300

    add screen backgroundColor attribute

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f'
+  }
+});
